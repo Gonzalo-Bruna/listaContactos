cc2b69aef2816049b66cb019b0e18e270d9bae91
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-04-27 10:34:46 -0400

    añadido las routes y configurado vistaCarta como el principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdicionarContactoComponent } from '../app/aplicacion/adicionar-contacto/adicionar-contacto.component';
+import { VistaCartaComponent } from '../app/aplicacion/vista-carta/vista-carta.component';
+import { VistaListaComponent } from '../app/aplicacion/vista-lista/vista-lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: VistaCartaComponent},
+  {path: 'vistaCarta', component: VistaListaComponent},
+  {path:'adicionarContacto', component: AdicionarContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
