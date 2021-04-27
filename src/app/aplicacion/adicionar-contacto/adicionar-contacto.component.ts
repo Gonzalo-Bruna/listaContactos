9be6b2a861365d8680047d5cf9fade2435082496
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-04-27 17:09:19 -0400

    hacer el formulario de tipo formgroup y formcontrol para todos sus elementos

--- src/app/aplicacion/adicionar-contacto/adicionar-contacto.component.ts
+++ src/app/aplicacion/adicionar-contacto/adicionar-contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-adicionar-contacto',
@@ -7,9 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdicionarContactoComponent implements OnInit {
 
-  constructor() { }
+  listaPaises:Array<string>;
+  formulario:FormGroup;
+  nombres:AbstractControl;
+  apellidos:AbstractControl;
+  radio:AbstractControl;
+  pais:AbstractControl;
+  ciudad:AbstractControl;
+
+  constructor(public fb:FormBuilder) { 
+    this.formulario = this.fb.group({
+      nombres:['', [Validators.required]],
+      apellidos:['', [Validators.required]],
+      radio:['', [Validators.required]],
+      pais:['', [Validators.required]],
+      ciudad:['', [Validators.required]]
+    });
+
+    this.nombres = this.formulario.controls['nombres'];
+    this.apellidos = this.formulario.controls['apellidos'];
+    this.radio = this.formulario.controls['radio'];
+    this.pais = this.formulario.controls['pais'];
+    this.ciudad = this.formulario.controls['ciudad'];
+    
+    this.listaPaises = [
+      "Argentina",
+      "Bolivia",
+      "Brasil",
+      "Chile",
+      "Colombia",
+      "Costa Rica",
+      "Cuba",
+      "Ecuador",
+      "El Salvador",
+      "Guatemala",
+      "Honduras",
+      "México",
+      "Nicaragua",
+      "Panamá",
+      "Paraguay",
+      "Perú",
+      "República Dominicana",
+      "Uruguay",
+      "Venezuela"
+    ];
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  crear(){
+    console.log(this.pais.value);
+  }
+
+  cancelar(){
+    this.nombres.setValue("");
+    this.apellidos.setValue("");
+    this.radio.setValue("");
+    this.pais.setValue("");
+    this.ciudad.setValue("");
   }
 
 }
